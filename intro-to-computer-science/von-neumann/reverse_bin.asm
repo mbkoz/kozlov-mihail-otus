99603eaf2b645d6fbee8f995d0544549bf5c7952
Михаил Козлов <KozlovMB@sinara-group.com>
2023-05-17 13:25:30 +0500

    черновик решения задачи по развороту строки

--- intro-to-computer-science/von-neumann/reverse_bin.asm
+++ intro-to-computer-science/von-neumann/reverse_bin.asm
@@ -1,10 +1,35 @@
-SOME .STRINGZ "SOME"
-
 .ORIG x3000 ; start of user space
+; вывели приглашение на ввод
 LEA R0, PROMPT
 PUTS
-HALT
+; считаем адрес, в котором будет располагаться строка
+LD R1, STRING_PTR
+; считаем терминирующий символ
+LD R2 NEWLINE_NEG
+; "считаем" из ввода "конец строки"
+ADD R1, R1, #1
+AND R0, R0, #0
+; читаем символы пользователя, пока не будет введен enter (контроль длины строки)
+; ЦИКЛ продолжается пока не будет введен ВВОД (символ CR)
+; по итогу ввода 
+READ_SYM
+ADD R1, R1, #-1
+STR R0, R1, #0
+GETC
+OUT
+; сравниваем с CR+LF
+ADD R3, R0, R2 
+BRnp READ_SYM
+; выведем введенные символы в обратном порядке
+AND R0, R0, #0
+ADD R0, R0, R1
+PUTS
 
-PROMPT .STRINGZ "ENTER BINARY VALUE: "
+HALT    ; Всегда должно быть при завершении программы
+
+PROMPT          .STRINGZ    "ENTER BINARY VALUE: "  ; метка, с записанным приглашением на ввод
+STRING_PTR      .FILL       0xF00                   ; метка, с указателем на область в которой будет
+                                                    ; размещаться строка
+NEWLINE_NEG     .FILL       x-000A                  ; -"символ конца строки"
 
 .END
