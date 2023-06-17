9d2d497966a01126dec3004678d72d4f657739fe
Mihail Kozlov <m_kozlov_1992@mail.ru>
2023-06-17 20:15:50 +0500

    добавлено черновое решение 3 задачи

--- intro-to-computer-science/low-level-programming/wday_search.asm
+++ intro-to-computer-science/low-level-programming/wday_search.asm
@@ -4,12 +4,39 @@ section .data
     arr dd  17, 21, 13, 17, 14, 12
     n   dd  6
 
+align       4
+stack       times 1024 dd 0;
+stack_top
+
 section .text
     global _start
     
 _start:
+    ; инициализация регистров и стека
+    xor eax, eax
+    xor edx, edx
+    mov ecx, [n]
+    mov ebx, arr
+    ; инициализация стека
+    mov esp, stack_top
+    push -1;    получаетя, что -1 - самая жаркая погода SE(0xFF)
 
-    ; Ваш код
-    ; Вам нужно будет вызвать print_number n раз
+    cmp ecx, 0x00
+    je final
 
+; отматываем с вершины стека значения температур, пока не найлем более теплый день
+search_wd:
+    pop eax
+    cmp eax, [ebx]
+    ja print_temp  ; используем "беззнаквый" режим сравнения (if above)
+    jmp search_wd
+; выводим найденный "более теплый" день и сохраняем новое значение температуры в стек
+print_temp:
+    call print_number
+    push eax
+    mov eax, [ebx]
+    push eax
+    add ebx, 4
+    loop search_wd
+final:
     call exit
