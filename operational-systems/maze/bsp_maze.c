2f89673ef9c722e4639662fad39931d626bd7095
Mihail Kozlov <m_kozlov_1992@mail.ru>
2023-07-11 11:00:02 +0500

    доделано решение с генерацией комнат

--- operational-systems/maze/bsp_maze.c
+++ operational-systems/maze/bsp_maze.c
@@ -8,19 +8,10 @@
 #include <inttypes.h>
 #include <time.h>
 
-/**
- * параметры командной строки:
- * 1. размер поля
- * 2. минимальный размер деления
- * 3. максимальный размер деления
- * 4. вероятность разбить комнату большего размера на 2 меньше
- * 5. размер padding'а
-*/
-
 enum {
     DEFAULT_HEIGHT = 100,
     DEFAULT_WIDTH = 100,
-    DEFAULT_MIN_LEAF_SIZE = 6,  // DEFAULT_MIN_LEAF_SIZE / 2 - минимальный размер комнаты
+    DEFAULT_MIN_LEAF_SIZE = 20,  // DEFAULT_MIN_LEAF_SIZE / 2 - минимальный размер комнаты
     MIN_ROOM_SIZE = DEFAULT_MIN_LEAF_SIZE / 2,
     DEFAULT_MAX_LEAF_SIZE = 20,
     DEFAULT_MIN_ROOM_MARGIN = 1 // 2x + MIN_ROOM_SIZE не должны превышать DEFAULT_MIN_LEAF_SIZE
@@ -39,7 +30,6 @@ typedef struct sColour {
 
 
 static const Colour LIGHT_COLOUR = {.r_ = 255, .g_ = 255, .b_ = 255};
-//static const Colour DARK_COLOUR = {.r_ = 0, .g_ = 0, .b_ = 0};
 
 typedef struct sRectangle {
     uint16_t x_;
@@ -66,11 +56,114 @@ static inline void printLightRectangle(FILE* file, const Rectangle r) {
     printColouredRectangle(file, r, LIGHT_COLOUR);
 }
 
-static inline void setMargin(Rectangle* const r, uint16_t margin){
-    r->x_ += margin;
-    r->y_ += margin;
-    r->w_ -= 2 * margin;
-    r->h_ -= 2 * margin;
+typedef struct sRoomsHadnler {
+    Rectangle* arr_;
+    size_t size_;
+} RoomsHadnler;
+
+void RH_init(RoomsHadnler* const self, size_t size);
+void RH_deinit(RoomsHadnler* const self);
+
+void RH_addRoom(RoomsHadnler* const self,Rectangle r);
+void RH_calculateHalls(RoomsHadnler* const self);
+void createHall(Rectangle lroom, Rectangle rroom);
+
+void RH_init(RoomsHadnler* const self, size_t maxSize) {
+    self->arr_ = malloc (sizeof(Rectangle) * maxSize);
+    self->size_ = 0;
+}
+
+void RH_deinit(RoomsHadnler* const self) {
+    free(self->arr_);
+}
+
+void RH_addRoom(RoomsHadnler* const self,Rectangle r) {
+    self->arr_[self->size_++] = r;
+}
+
+void RH_calculateHalls(RoomsHadnler* const self) {
+    for(size_t i = 1; i < self->size_; ++i)
+        createHall(self->arr_[i - 1], self->arr_[i]);
+}
+
+
+void createHall(Rectangle lroom, Rectangle rroom) {
+    typedef struct sPoint {
+        uint16_t x_;
+        uint16_t y_;
+    } Point;
+
+    Point point1 = (Point){
+        .x_ = lroom.x_ + 1 + rand() % (lroom.w_ - 2),
+        .y_ = lroom.y_ + 1 + rand() % (lroom.h_ - 2),
+    };
+
+    Point point2 = (Point){
+        .x_ = rroom.x_ + 1 + rand() % (rroom.w_ - 2),
+        .y_ = rroom.y_ + 1 + rand() % (rroom.h_ - 2),
+    };
+
+    int32_t w = point2.x_ - point1.x_;
+    int32_t h = point2.y_ - point1.y_;
+
+    if (w < 0) {
+        if (h < 0) {
+            if (rand() % 1000 < 500) {
+                printLightRectangle(stdout, (Rectangle){point2.x_, point1.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, 1, abs(h) + 1});
+            }
+            else {
+                printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point1.x_, point2.y_, 1, abs(h) + 1});
+            }
+        }
+        else if (h > 0) {
+            if (rand() % 1000 < 500) {
+                printLightRectangle(stdout, (Rectangle){point2.x_, point1.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point2.x_, point1.y_, 1, abs(h) + 1});
+            }
+            else {
+                printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, 1, abs(h) + 1});
+            }
+        }
+        else {
+            printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, abs(w) + 1, 1});
+        }
+    }
+    else if (w > 0) {
+        if (h < 0) {
+            if (rand() % 1000 < 500) {
+                printLightRectangle(stdout, (Rectangle){point1.x_, point2.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point1.x_, point2.y_, 1, abs(h) + 1});
+            }
+            else {
+                printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, 1, abs(h) + 1});
+            }
+        }
+        else if (h > 0) {
+            if (rand() % 1000 < 500) {
+                printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point2.x_, point1.y_, 1, abs(h) + 1});
+            }
+            else {
+                printLightRectangle(stdout, (Rectangle){point1.x_, point2.y_, abs(w) + 1, 1});
+                printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, 1, abs(h) + 1});
+            }
+        }
+        else {
+            printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, abs(w) + 1, 1});
+        }
+    }
+    else {
+        if (h < 0) {
+            printLightRectangle(stdout, (Rectangle){point2.x_, point2.y_, 1, abs(h) + 1});
+        }
+        else if (h > 0) {
+            printLightRectangle(stdout, (Rectangle){point1.x_, point1.y_, 1, abs(h) + 1});
+        }
+    }
 }
 
 /**
@@ -79,8 +172,6 @@ static inline void setMargin(Rectangle* const r, uint16_t margin){
 
 typedef struct sLeaf {
     Rectangle leafr_;
-    Rectangle roomr_;
-    //Rectangle halls_[10];
     struct sLeaf* lChild_;
     struct sLeaf* rChild_;
 } Leaf;
@@ -91,14 +182,15 @@ void Leaf_init(Leaf* const self, Rectangle const leafr);
 Leaf* Leaf_create(Rectangle const leafr);
 void Leaf_destroy(Leaf* const leaf);
 bool Leaf_split(Leaf* const self);
-void Leaf_splitToPrimitives(Leaf* const self);
-void Leaf_forEachTopLeaf(Leaf* const, ModifyLeafFunctionT modifyLeaf);
+void Leaf_splitToPrimitives(Leaf* const self, RoomsHadnler* rh);
+//void Leaf_makeRoomForTopLeaf(Leaf* const);
+//Rectangle Leaf_getRoom(const Leaf* self);
 
-void createRoom(Leaf* const leaf);
+void createRoom(Leaf* const leaf, RoomsHadnler* rh);
+
 
 void Leaf_init(Leaf* const self, Rectangle const leafr) {
     self->leafr_ = leafr;
-    self->roomr_ = (Rectangle){0};
     self->lChild_ = NULL;
     self->rChild_ = NULL;
 }
@@ -119,17 +211,12 @@ void Leaf_destroy(Leaf* const leaf) {
     free(leaf);
 }
 
+// разделение на 2 потомка
 bool Leaf_split(Leaf* const self) {
-    // begin splitting the leaf into two children
-
     if(self->lChild_ != NULL || self->rChild_ != NULL)
         return false; // we're already split! Abort!
 
-    // determine direction of split
-    // if the width is >25% larger than height, we split vertically
-    // if the height is >25% larger than the width, we split horizontally
-    // otherwise we split randomly
-
+    // определим способ разделения (по вертикали или по горизонтали)
     bool splitH;
 
     if ((float)(self->leafr_.w_) / self->leafr_.h_ >= 1.25)
@@ -139,13 +226,13 @@ bool Leaf_split(Leaf* const self) {
     else
         splitH = (rand() % 1000 >= 500);
 
-    // check MIN_LEAF_SIZE
+    // проверка того, что части не будут меньше минимального порога
     uint16_t max = (splitH ? self->leafr_.h_ : self->leafr_.w_) - DEFAULT_MIN_LEAF_SIZE;
     if (max <= DEFAULT_MIN_LEAF_SIZE)
-        return false; // the area is too small to split any more...
+        return false;
 
     uint16_t split = rand() % (max - DEFAULT_MIN_LEAF_SIZE + 1) + DEFAULT_MIN_LEAF_SIZE; // determine where we're going to split
-    // create our left and right children based on the direction of the split
+    // разделение на части
     if (splitH) {
         self->lChild_ = Leaf_create((Rectangle){self->leafr_.x_, self->leafr_.y_, self->leafr_.w_, split});
         self->rChild_ = Leaf_create((Rectangle){self->leafr_.x_, self->leafr_.y_ + split, self->leafr_.w_, self->leafr_.h_ - split});
@@ -154,48 +241,77 @@ bool Leaf_split(Leaf* const self) {
         self->rChild_ = Leaf_create((Rectangle){self->leafr_.x_ + split, self->leafr_.y_, self->leafr_.w_ - split, self->leafr_.h_});
     }
 
-    return true; // split successful!
+    return true; // успех
 }
 
-void Leaf_splitToPrimitives(Leaf* const self) {
+// разделить рекурсивно
+void Leaf_splitToPrimitives(Leaf* const self, RoomsHadnler* rh) {
     if(self->leafr_.w_ > DEFAULT_MAX_LEAF_SIZE || self->leafr_.h_ > DEFAULT_MAX_LEAF_SIZE || rand() % 1001 > 250) {
         if(Leaf_split(self)) {
-            Leaf_splitToPrimitives(self->lChild_);
-            Leaf_splitToPrimitives(self->rChild_);
+            Leaf_splitToPrimitives(self->lChild_, rh);
+            Leaf_splitToPrimitives(self->rChild_, rh);
         }
+        else
+            createRoom(self, rh);
     }
+    else
+        createRoom(self, rh);
 }
 
-void Leaf_forEachTopLeaf(Leaf* const leaf, ModifyLeafFunctionT modifyLeaf) {
-    if(leaf->lChild_)
-        Leaf_forEachTopLeaf(leaf->lChild_, modifyLeaf);
-    if(leaf->rChild_)
-        Leaf_forEachTopLeaf(leaf->rChild_, modifyLeaf);
-
-    if(!leaf->lChild_ && !leaf->rChild_)
-        modifyLeaf(leaf);
-}
-
-void createRoom(Leaf* const leaf) {
+void createRoom(Leaf* const leaf, RoomsHadnler* rh) {
     Rectangle r;
     r.w_ = rand() % (leaf->leafr_.w_ - 2 * DEFAULT_MIN_ROOM_MARGIN - MIN_ROOM_SIZE + 1) + MIN_ROOM_SIZE;
     r.h_ = rand() % (leaf->leafr_.h_ - 2 * DEFAULT_MIN_ROOM_MARGIN - MIN_ROOM_SIZE + 1) + MIN_ROOM_SIZE;
     r.x_ = leaf->leafr_.x_ + DEFAULT_MIN_ROOM_MARGIN + rand() % (leaf->leafr_.w_ - 2 * DEFAULT_MIN_ROOM_MARGIN - r.w_ + 1);
     r.y_ = leaf->leafr_.y_ + DEFAULT_MIN_ROOM_MARGIN + rand() % (leaf->leafr_.h_ - 2 * DEFAULT_MIN_ROOM_MARGIN - r.h_ + 1);
-    leaf->roomr_ = r;
     printLightRectangle(stdout, r);
+    RH_addRoom(rh, r);
 }
+
+//Rectangle Leaf_getRoom(const Leaf* self) {
+//    // iterate all the way through these leafs to find a room, if one exists.
+//    if (self->lChild_ == NULL && self->rChild_ == NULL) // 
+//        return self->roomr_;
+//    else {
+//        Rectangle lroom;
+//        Rectangle rroom;
+//
+//        if (self->lChild_ != NULL) {
+//            lroom = Leaf_getRoom(self->lChild_);
+//        }
+//        if (self->rChild_ != NULL) {
+//            rroom = Leaf_getRoom(self->rChild_);
+//        }
+//        if (lroom.w_ == 0 && rroom.w_ == 0)
+//            return (Rectangle){0};
+//        else if (rroom.w_ == 0)
+//            return lroom;
+//        else if (lroom.w_ == 0)
+//            return rroom;
+//        else if (rand() % 1000 > 499)
+//            return lroom;
+//        else
+//            return rroom;
+//    }
+//
+//}
+
+
 
 int main(int argc, char* argv[]) {
     printf("map %"PRIu32" %"PRIu32" 0 0 0\n", DEFAULT_WIDTH, DEFAULT_HEIGHT);
     srand(time(NULL));
 
     Leaf* root = Leaf_create((Rectangle){0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT});
+    RoomsHadnler rh;
+    RH_init(&rh, (DEFAULT_WIDTH * DEFAULT_HEIGHT) / (DEFAULT_MIN_LEAF_SIZE * DEFAULT_MIN_LEAF_SIZE));
 
-    Leaf_splitToPrimitives(root);
-    Leaf_forEachTopLeaf(root, createRoom);
+    Leaf_splitToPrimitives(root, &rh);
+    //Leaf_makeRoomForTopLeaf(root);
+    RH_calculateHalls(&rh);
 
     Leaf_destroy(root);
+    RH_deinit(&rh);
 
     return 0;
 }
